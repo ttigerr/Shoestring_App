f5f23f37c58f7532855ee9e06db2b609cc615dcd
Tiger <tiger.chalernphon15@gmail.com>
2022-08-17 18:23:15 +1000

    commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import LandingPage from './src/components/LandingPage';
 import Results from './src/components/Results';
 import Login from './src/components/Login';
 import Details from './src/components/Details';
+
 import Register from './src/components/Register';
 import MyBookings from "./src/components/MyBookings"
 import BookingDetails from './src/components/BookingDetails';
