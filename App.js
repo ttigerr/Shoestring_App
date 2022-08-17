67a724365b9b326982b3bce7c5be4f9f26341d6b
Tiger <tiger.chalernphon15@gmail.com>
2022-08-17 15:35:11 +1000

    new commits

--- App.js
+++ App.js
@@ -1,128 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// Import components
-
-import { Login } from './components/Login';
-import { Register } from './components/Register';
-import { LandingPage } from './components/LandingPage';
-
-// Initialize the stack
-const Stack = createNativeStackNavigator()
-
-// Import Firebase
-import { initializeApp } from 'firebase/app'
-import { firebaseConfig } from './Config'
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
-import { initializeFirestore, getFirestore, setDoc, doc, addDoc, getDoc, query, onSnapshot, collection } from 'firebase/firestore'
-
-// Initialize the firebase variable
-const FBapp = initializeApp( firebaseConfig)
-const FSdb = initializeFirestore(FBapp, {useFetchStreams: false})
-const FBauth = getAuth()
-
-
-// Main application
-export default function App() {
-
-  // Initialize
-const[ auth, setAuth ] = useState()
-const[ user, setUser ] = useState()
-const [ data, setData ] = useState()
-const [registerError, setRegisterError ] = useState()
-const [loginError, setLoginError ] = useState()
-
-// useEffect(() => {
-//   const unsubscribe = onAuthStateChanged( FBauth, (user) => {
-//     if( user ) { 
-//       setAuth(true) 
-//       setUser(user)
-//       if( !data ) { getData() }
-//     }
-//     else {
-//       setAuth(false)
-//       setUser(null)
-//     }
-//   })
-//   unsubscribe()
-// })
-
-// get user's auth status
-const getAuthStatus = () => {
-  return new Promise( ( resolve, reject ) => {
-    if( FBauth.currentUser ) {
-      resolve( true )
-    }
-    else {
-      reject( false )
-    }
-  })
-}
-
-// Registration
-const RegisterHandler = ( email, password ) => {
-  setRegisterError(null)
-  createUserWithEmailAndPassword( FBauth, email, password )
-  .then( ( userCredential ) => { 
-    setUser(userCredential.user)
-    setAuth( true )
-  } )
-  .catch( (error) => { setRegisterError(error.code) })
-}
-
-// Log in
-const LoginHandler = ( email, password ) => {
-  signInWithEmailAndPassword( FBauth, email, password )
-  .then( (userCredential) => {
-    setUser(userCredential.user)
-    setAuth(true)
-  })
-  .catch( (error) => { 
-    const message = (error.code.includes('/') ) ? error.code.split('/')[1].replace(/-/g, ' ') : error.code
-    setLoginError(message) 
-  })
-}
-
-// // Add users in DB
-// const addUserData = async( FScollection , data ) => {
-//   const ref = await addDoc( collection( FSdb, FScollection) , data )
-
-// }
-
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingPage from './src/components/LandingPage';
+import Results from './src/components/Results';
+import Login from './src/components/Login';
+import Details from './src/components/Details';
+import Register from './src/components/Register';
+import MyBookings from "./src/components/MyBookings"
+import BookingDetails from './src/components/BookingDetails';
+const Stack = createStackNavigator();
+import BookingDetail from "./src/components/BookingDetail"
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Login" options={{title:'Log In'}}>
-            { (props) => 
-                <Login {...props} 
-                handler={LoginHandler} 
-                auth={auth} 
-                error={loginError} 
-            /> }
-        </Stack.Screen>
-        <Stack.Screen name="Register" options={{title: 'Registration'}}>
-          { (props) => 
-            <Register {...props} 
-            handler={RegisterHandler} 
-            auth={auth} 
-            error={registerError} 
-          /> }
-        </Stack.Screen>
-        <Stack.Screen name="LandingPage" component={LandingPage}  />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name='BookingDetails' component={BookingDetails} />
+        <Stack.Screen name="Results" component={Results} /> 
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="MyBookings" component={MyBookings} />
+        <Stack.Screen name="BookingDetail" component={BookingDetail} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-// Style
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
